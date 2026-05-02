43292fc09c3c3eadc39bb3e8971989c6a44cbb70
Skickle <168455940+GabrielNFR@users.noreply.github.com>
2026-05-02 11:15:17 -0300

    Corrige repositório duplo adicionando src corretamente

---
+++ src/client.py
@@ -0,0 +1,136 @@
+import socket
+import json
+import sys
+import select, time
+from utils import fragmentar_e_montar, calcular_checksum
+
+def main():
+    if len(sys.argv) != 3:
+        print("Uso correto: python src/client.py <IP> <PORTA>")
+        sys.exit(1)
+
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(5.0)
+
+    print("CONFIGURAÇÃO INICIAL")
+    print("Escolha um modo de operação:")
+    print("[1] Go-Back-N")
+    print("[2] Repetição Seletiva")
+
+    while True:
+        modo = input("Escolha o modo (1 ou 2): ")
+        if modo == "1":
+            modo_escolhido = "Go-Back-N"
+            break
+        elif modo == "2":
+            modo_escolhido = "Repetição Seletiva"
+            break
+        else:
+            print("Opção inválida, escolha 1 ou 2.")
+
+    tam_max = int(input("Digite o limite máximo de caracteres por envio (Mínimo de 30): "))
+    if tam_max < 30:
+        tam_max = 30
+
+    handshake_msg = {
+        "tipo": "HANDSHAKE",
+        "modo": modo_escolhido,
+        "tam_max": tam_max
+    }
+
+    print(f"Iniciado Handshake com o servidor {IP}:{PORT}...")
+    sock.sendto(json.dumps(handshake_msg).encode('utf-8'), (IP, PORT))
+
+    try:
+        data, addr = sock.recvfrom(1024)
+        resposta = json.loads(data.decode('utf-8'))
+
+        if resposta.get("tipo") == "HANDSHAKE_ACK":
+            janela_atual = resposta.get("janela_inicial")
+            status = resposta.get("status")
+
+            print("Conexão estabelecida com sucesso.")
+            print(f"Servidor aceitou com janela: {janela_atual}")
+            print(f"Status: {status}")
+
+            while True:
+                mensagem_usuario = input("Digite a mensagem: ")
+
+                if len(mensagem_usuario) > tam_max:
+                    print("Erro: Mensagem muito grande.")
+                    continue
+
+                lista_pdus = fragmentar_e_montar(mensagem_usuario)
+                
+                print(f"\n[FASE 1 OK] Foram gerados {len(lista_pdus)} pacotes:")
+                for p in lista_pdus:
+                    print(f"  -> Pacote #{p['seq_num']} | Payload: '{p['payload']}' | Checksum: {p['checksum'][:10]}...")
+                print("-" * 40)
+
+                base_janela = 0
+                proximo_seq_num = 0
+                total_pacotes = len(lista_pdus)
+                
+                timers = {} 
+                pacotes_ack_recebidos = set() 
+
+                while base_janela < total_pacotes:
+                    
+                    tempo_atual = time.time()
+                    limite_janela = min(base_janela + janela_atual, total_pacotes)
+                    
+                    for i in range(base_janela, limite_janela):
+                        if i == proximo_seq_num:
+                            pacote = lista_pdus[i]
+                            sock.sendto(json.dumps(pacote).encode('utf-8'), (IP, PORT))
+                            print(f"[>] Enviado pacote #{pacote['seq_num']}")
+                            timers[i] = time.time()
+                            proximo_seq_num += 1
+                            
+                        elif i < proximo_seq_num and i not in pacotes_ack_recebidos:
+                            if (tempo_atual - timers.get(i, tempo_atual)) > 5.0:
+                                if modo_escolhido == "Repetição Seletiva":
+                                    print(f"    [TIMEOUT SR] Pacote avulso #{i} falhou. Reenviando apenas o #{i}.")
+                                    pacote = lista_pdus[i]
+                                    sock.sendto(json.dumps(pacote).encode('utf-8'), (IP, PORT))
+                                    timers[i] = time.time() 
+                                    
+                                elif modo_escolhido == "Go-Back-N" and i == base_janela:
+                                    print(f"    [TIMEOUT GBN] Base #{base_janela} falhou. Retransmitindo janela inteira do começo.")
+                                    proximo_seq_num = base_janela 
+                                    timers[base_janela] = time.time()
+                                    break 
+
+                    pronto_para_ler, _, _ = select.select([sock], [], [], 0.1)
+
+                    if pronto_para_ler:
+                        data_ack, _ = sock.recvfrom(1024)
+                        ack = json.loads(data_ack.decode('utf-8'))
+                        
+                        if ack.get("tipo") == "ACK":
+                            seq_ack = ack.get("seq_num")
+                            print(f"    [<] Recebido ACK para o pacote #{seq_ack}")
+                            
+                            if modo_escolhido == "Go-Back-N":
+                                if seq_ack >= base_janela:
+                                    base_janela = seq_ack + 1
+                                    
+                            elif modo_escolhido == "Repetição Seletiva":
+                                pacotes_ack_recebidos.add(seq_ack)
+                                while base_janela in pacotes_ack_recebidos:
+                                    base_janela += 1
+
+                fin_msg = {
+                    "tipo": "FIN"
+                }
+                sock.sendto(json.dumps(fin_msg).encode('utf-8'), (IP, PORT))
+                print("Mensagem finalizada (FIN enviado).")
+
+    except socket.timeout:
+        print("TIMEOUT no handshake")
+
+if __name__ == "__main__":
+    main()
---
+++ src/server.py
@@ -0,0 +1,96 @@
+import socket
+import json
+import sys
+from utils import calcular_checksum
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Uso correto: python src/server.py <IP> <PORTA>")
+        sys.exit(1)
+
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP, PORT))
+
+    print(f"Servidor escutando em {IP}:{PORT}")
+
+    buffer_mensagens = {}
+    cliente_ativo = None
+    modo_operacao = None
+    seq_esperado = 0
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        mensagem = json.loads(data.decode('utf-8'))
+
+        if mensagem.get("tipo") == "HANDSHAKE":
+            print(f"\nHandshake recebido de {addr}")
+            print(f"Modo: {mensagem.get('modo')}")
+            print(f"Tam_max: {mensagem.get('tam_max')}")
+
+            cliente_ativo = addr
+            buffer_mensagens = {}
+            
+            modo_operacao = mensagem.get("modo")
+            seq_esperado = 0
+            
+            resposta = {
+                "tipo": "HANDSHAKE_ACK",
+                "janela_inicial": 5,
+                "status": "ACEITO"
+            }
+
+            sock.sendto(json.dumps(resposta).encode('utf-8'), addr)
+            print("Resposta enviada.")
+
+        elif mensagem.get("tipo") == "DATA":
+            seq_num = mensagem.get('seq_num')
+            payload = mensagem.get('payload')
+            checksum = mensagem.get('checksum')
+
+            print(f"\nRecebido pacote DATA #{seq_num}, payload: '{payload}'")
+
+            if calcular_checksum(payload) != checksum:
+                print(f"[ERRO] Checksum inválido no pacote #{seq_num}")
+                continue
+            
+            if modo_operacao == "Go-Back-N":
+                if seq_num == seq_esperado:
+                    buffer_mensagens[seq_num] = payload
+                    seq_esperado += 1
+
+                    ack = {
+                        "tipo": "ACK",
+                        "seq_num": seq_num
+                    }
+                else:
+                    ack = {
+                        "tipo": "ACK",
+                        "seq_num": seq_esperado - 1
+                    }
+
+            elif modo_operacao == "Repetição Seletiva":
+                if seq_num not in buffer_mensagens:
+                    buffer_mensagens[seq_num] = payload
+
+                ack = {
+                    "tipo": "ACK",
+                    "seq_num": seq_num
+                }
+
+            sock.sendto(json.dumps(ack).encode('utf-8'), addr)
+            print(f"ACK enviado para o pacote #{seq_num}")
+
+        elif mensagem.get("tipo") == "FIN":
+            if buffer_mensagens:
+                chaves_ordenadas = sorted(buffer_mensagens.keys())
+                mensagem_completa = ''.join(buffer_mensagens[k] for k in chaves_ordenadas)
+                print(f"\nMensagem completa recebida: '{mensagem_completa}'")
+            
+            buffer_mensagens = {}
+
+if __name__ == "__main__":
+    main()
